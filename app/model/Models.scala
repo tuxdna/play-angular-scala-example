adf487a160bd55e4703ba7e72cff8bf484fcc6c3
Saleem Ansari <tuxdna@gmail.com>
2015-07-07 22:47:17 +0530

    Default to inmemory db

--- app/model/Models.scala
+++ app/model/Models.scala
@@ -3,8 +3,9 @@ package model
 import scala.slick.lifted.Tag
 import java.sql.Timestamp
 
-// import play.api.db.slick.Config.driver.simple._
-import scala.slick.driver.MySQLDriver.simple._
+import play.api.db.slick.Config.driver.simple._
+
+// import scala.slick.driver.MySQLDriver.simple._
 
 case class Customer(
   id: Option[Int],
@@ -172,6 +173,71 @@ object Tickets {
 
     val tk = get(ticketId)
     tk
+  }
+
+}
+
+import play.api.db.slick.DB
+import play.api.Application
+//import scala.slick.jdbc.meta.MTable
+//
+//// import play.api.db.slick.Config.driver.simple._
+//import scala.slick.driver.MySQLDriver.simple._
+
+object Schema {
+  val tickets = TableQuery[Tickets]
+  val customers = TableQuery[Customers]
+  val csrs = TableQuery[CustomerServiceRepresentatives]
+  val comments = TableQuery[Comments]
+  
+  val ddl = (customers.ddl ++
+    comments.ddl ++
+    csrs.ddl ++
+    tickets.ddl)
+
+  def generateSchema(implicit app: Application) {
+    println("Populate the application schema...")
+
+    DB.withSession { session =>
+
+      println("-- Create tables SQL ...")
+      for (sql <- Schema.ddl.createStatements) {
+        println(s"${sql};")
+      }
+
+      println("-- Drop tables SQL...")
+      for (sql <- Schema.ddl.dropStatements.toList.reverse) {
+        println(s"${sql};")
+      }
+
+    }
+  }
+
+  def populateSchema(implicit app: Application) {
+    println("Populate the application schema...")
+
+    DB.withSession { session =>
+
+      println("Create tables...")
+      for (s <- Schema.ddl.createStatements) {
+        val conn = session.conn
+        val stmt = conn.createStatement()
+        stmt.execute(s)
+        stmt.close()
+      }
+
+    }
+  }
+
+  def hasGoldData(implicit app: Application) = {
+    try {
+      DB.withSession { implicit session =>
+        val tickets = Tickets.list
+        tickets.length > 0
+      }
+    } catch {
+      case e: Throwable => false
+    }
   }
 
 }
--- build.sbt
+++ build.sbt
@@ -17,7 +17,8 @@ libraryDependencies ++= Seq(
   "com.typesafe.play" %% "play-slick" % "0.6.0.1",
   "com.typesafe.slick" %% "slick-extensions" % "2.0.0",
   "mysql" % "mysql-connector-java" % "5.1.31",
-  "org.xerial" % "sqlite-jdbc" % "3.7.2"
+  "org.xerial" % "sqlite-jdbc" % "3.7.2",
+  "postgresql" % "postgresql" % "9.1-901.jdbc4"
 )
 
 play.Project.playScalaSettings
